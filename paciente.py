76ba88bbe4e0bc71221ad3e95def959f09f59ff3
Daniel <danielsalazar_07@hotmail.com>
2025-10-14 00:42:38 -0500

    Update paciente.py

--- paciente.py
+++ paciente.py
@@ -2,17 +2,29 @@ from persona import Persona
 
 class Paciente(Persona):
     def __init__(self, dni, nombre, apellido, telefono, direccion):
+        
         super().__init__(dni, nombre, apellido, telefono, direccion)
 
         self.historial_citas = []
         self.prescripciones = []
 
-    def registrarPrescripcion():
-        pass
+    def agregar_cita(self, cita):
+        
+        self.historial_citas.append(cita)
+        print(f"Cita para {self.nombre_completo} agendada correctamente.")
 
-    def agendarCitas():
-        pass
+    def agregar_prescripcion(self, prescripcion):
+       
+        self.prescripciones.append(prescripcion)
+        print(f"Prescripción registrada para {self.nombre_completo}.")
 
-
-    def verFicha():
-        pass
+    def __str__(self):
+        
+        info_persona = super().__str__()
+        
+       
+        info_paciente = (f"\n--- Historial del Paciente ---\n"
+                         f"Número de citas: {len(self.historial_citas)}\n"
+                         f"Número de prescripciones: {len(self.prescripciones)}")
+        
+        return info_persona + info_paciente
