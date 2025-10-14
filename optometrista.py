f75a85248581836e13427e96460f1fdcaa5eb2c4
Daniel <danielsalazar_07@hotmail.com>
2025-10-14 01:08:05 -0500

    Update optometrista.py

--- optometrista.py
+++ optometrista.py
@@ -1,7 +1,19 @@
 from persona import Persona
 
 class Optometrista(Persona):
-    def __init__(self, dni, nombre, apellido, telefono, direccion, especialidad):
-        super().__init__(dni, nombre, apellido,telefono, direccion)
+    def __init__(self, dni, nombre, apellido, telefono, direccion, id_medico, especialidad):
         
+        super().__init__(dni, nombre, apellido, telefono, direccion)
+
+        self.id_medico = id_medico
         self.especialidad = especialidad
+
+    def __str__(self):
+       
+        info_persona = super().__str__()
+        
+        info_optometrista = (f"\n--- Datos Profesionales ---\n"
+                             f"ID Médico: {self.id_medico}\n"
+                             f"Especialidad: {self.especialidad}")
+        
+        return info_persona + info_optometrista
