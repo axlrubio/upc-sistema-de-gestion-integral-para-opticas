a641ad1a17036e6b7b58b8d4874b832fa5b2d5b5
Daniel <danielsalazar_07@hotmail.com>
2025-10-14 00:33:37 -0500

    Update persona.py

--- persona.py
+++ persona.py
@@ -5,6 +5,21 @@ class Persona:
         self.apellido = apellido
         self.telefono = telefono
         self.direccion = direccion
+    
+    @property
+    
+    def nombre_completo(self):
+        return f"{self.nombre} {self.apellido}"
+    
+    def __str__(self):
+        
+        return (f"DNI: {self.dni}\n"
+                f"Nombre: {self.nombre} {self.apellido}\n"
+                f"Teléfono: {self.telefono}\n"
+                f"Dirección: {self.direccion}")
 
-    def modificar_datos(self):
-        pass
+    def modificar_datos(self, nuevo_telefono, nueva_direccion):
+        
+        self.telefono = nuevo_telefono
+        self.direccion = nueva_direccion
+        print("Datos actualizados correctamente.")
